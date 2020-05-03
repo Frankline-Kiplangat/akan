d3c6ce1e565162730f16e518ce3f8f4fdd660450
Frank <kipfrankline@gmail.com>
2020-05-03 13:28:28 +0300

    Added js

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var day;
+function dateOfWeek() {
+    var dateOfBirth= document.getElementById("dateOfBirth").Value;
+    var CC = parseInt(dateOfBirth.substring(0, 2));
+    var YY = parseInt(dateOfBirth.substring(2, 4));
+    var MM = parseInt(dateOfBirth.substring(5, 7));
+    var DD = parseInt(dateOfBirth.substring(8, 10));
+     d = (((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * (MM + 1)/10)) + DD) %7
+
+     function validate(){
+         ("datepicker").datepicker({maxDate: today});
+     }
+     day = Math.floor(d) - 1;
+     daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+     
+     var option = document.getElementsByName('name').values;
+     var maleGender = document.getElementsById("maleGender").value;
+     var femaleGender= document.getElementsById("femaleGender").value;
+     var akanMales= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+     var akanFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa",
+    "Afua", "Ama"];
+
+    if (option === "Male"){
+        document.getElementsById(akanOutput).innerHTML = "You were born on" + daysOfTheWeek[day] + "and your name is" + akanMales[day];
+        console.log(daysOfTheWeek[day]);
+        console.log(akanMales[day]);
+    } else if (option === "Female") {
+        document.getElementsById(akanOutput).innerHTML = "You were on" + daysOfTheWeek + "and your name is" + akanFemales[day];
+        console.log(daysOfTheWeek[day]);
+        console.log(akanFemales[day]);
+    } 
+    // else{
+    //     console.log(daysOfTheWeek);
+    // }
+
+
+}
