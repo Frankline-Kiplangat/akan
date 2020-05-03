61762101033fd2c4502abcf33af44de25ed23068
Frank <kipfrankline@gmail.com>
2020-05-03 22:06:37 +0300

    Add initial changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 // Backend logic //
 var daysOfTheWeek= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var akanMales= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var akanFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 // Frontend logic //
 function findAkanNames(){
-    var day = parseInt(document.getElementsById("day").value);
-    var day = parseInt(document.getElementsById("month").value);
-    var day = parseInt(document.getElementsById("year").value);
+    var day = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
     var male = document.getElementById("male");
-    var female = document.getElementsById("female");
+    var female = document.getElementById("female");
 
     if (day <= 0 || day > 31 ){
-        alert("Please enter a valid month!");
+        alert("Please enter a valid day!");
     } else if (month <= 0 ||  month > 12  || (month == 2 && day >29)){
-        alert("PLease enter a valid month!")
+        alert("Please enter a valid month!")
     }
 
     
-    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (5 + 1) / 10)) + 9) % 7;
-    var newDate = new Date( day + "/" + "month" + "/" + "year");
+
+    // d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (5 + 1) / 10)) + 9) % 7;
+
+
+    var newDate = new Date(day + "/" + month + "/" + year);
     var d = newDate.getDay();
-    if (option == "male"){
-        document.getElementsById('akan-names').innerHTML = "You were born on " + daysOfTheWeek[d] +  " and your Akan name is: " + akanMales[d] ;
-    } else if (option == "female"){
-        "You were born on " + daysOfTheWeek[d] +  " and your Akan name is: " + akanFemales[d] ;
+    if (male.checked == true){
+        alert("You were born on " + daysOfTheWeek[d] +  " and your Akan name is " + maleNames[d]);
+        // document.getElementById('akan-names').innerHTML = "You were born on " + daysOfTheWeek[d] +  " and your Akan name is " + maleNames[d];
+        console.log(daysOfTheWeek[d]);
+        console.log(maleNames[d]);
+    } else if (female.checked == true){
+        alert("You were born on " + daysOfTheWeek[d] +  " and your Akan name is " + femaleNames[d]);
+        console.log(daysOfTheWeek[d]);
+        console.log(femaleNames[d]);
     }
 }
 
