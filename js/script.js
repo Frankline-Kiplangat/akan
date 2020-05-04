e39dd6c64570fbbcf53841e5bee45e81216993a5
Frank <kipfrankline@gmail.com>
2020-05-04 08:59:53 +0300

    Add changes

--- js/script.js
+++ js/script.js
@@ -5,30 +5,26 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 // Frontend logic //
 function findAkanNames(){
-    var date = parseInt(document.getElementById("date").value);
+    var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
     var male = document.getElementById("male");
     var female = document.getElementById("female");
      
-    if (date <= 0 || date > 31 ){
+    if (day <= 0 || day > 31 ){
         alert("Please enter a valid date!");
-    } else if (month <= 0 ||  month > 12  || (month == 2 && date >29)){
+    } else if (month <= 0 ||  month > 12  || (month == 2 && day >29)){
         alert("Please enter a valid month!")
     } else if(year <= 0){
         alert("Please enter a valid year")
     }
 
-    function date() {
-        ("datepicker").datepicker({maxDate: today});
-      }
-      day = Math.floor(d)-1;
-
-    // d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (5 + 1) / 10)) + 9) % 7; //
+    var a = Math.floor((14 - month) / 12)
+    var y = year - a
+    var m = month + 12 * a - 2
+    var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m )/ 12)) % 7;
 
 
-    var newDate = new Date(date + "/" + month + "/" + year);
-    var d = newDate.getDate();
     if (male.checked == true){
         alert("You were born on " + daysOfTheWeek[d] +  " and your Akan name is " + maleNames[d]);
         console.log(daysOfTheWeek[d]);
@@ -41,103 +37,3 @@ function findAkanNames(){
         alert("Select gender")
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var day;
-// function dateOfWeek() {
-//     var dateOfBirth= document.getElementById("dateOfBirth").Value;
-//     var CC = parseInt(dateOfBirth.substring(0, 2));
-//     var YY = parseInt(dateOfBirth.substring(2, 4));
-//     var MM = parseInt(dateOfBirth.substring(5, 7));
-//     var DD = parseInt(dateOfBirth.substring(8, 10));
-//      d = (((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * (MM + 1)/10)) + DD) %7
-
-//      function validate(){
-//          ("datepicker").datepicker({maxDate: today});
-//      }
-//      day = Math.floor(d) - 1;
-//      daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-     
-//      var option = document.getElementsByName('name').values;
-//      var male = document.getElementsById("Male").value;
-//      var female= document.getElementsById("Female").value;
-//      var akanMales= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-//      var akanFemales = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa",
-//     "Afua", "Ama"];
-
-//     if (option == "Male"){
-//         document.getElementsById(akanOutput).innerHTML = "You were born on" + daysOfTheWeek[day] + "and your name is" + akanMales[day];
-//         console.log(daysOfTheWeek[day]);
-//         console.log(akanMales[day]);
-//     } else if (option == "Female") {
-//         document.getElementsById(akanOutput).innerHTML = "You were on" + daysOfTheWeek + "and your name is" + akanFemales[day];
-//         console.log(daysOfTheWeek[day]);
-//         console.log(akanFemales[day]);
-//     } 
-    
-
-// }
